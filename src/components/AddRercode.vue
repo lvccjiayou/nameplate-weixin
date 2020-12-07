a0a9c0e9397d3e968d6c1ca0274cd7fe94b66b06
lvgang <1512499524@qq.com>
2020-12-07 18:36:53 +0800

    扫码接口，方法编写

--- src/components/AddRercode.vue
+++ src/components/AddRercode.vue
@@ -210,12 +210,12 @@
         console.log("调用扫一扫接口")
         wx.config({
           beta: true,// 必须这么写，否则wx.invoke调用形式的jsapi会有问题
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: '', // 必填，企业微信的corpID
           timestamp: '', // 必填，生成签名的时间戳
           nonceStr: '', // 必填，生成签名的随机串
           signature: '',// 必填，签名，见 附录-JS-SDK使用权限签名算法
-          jsApiList: [] // 必填，需要使用的JS接口列表，凡是要调用的接口都需要传进来
+          jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表，凡是要调用的接口都需要传进来
         });
         wx.ready(function(){
           // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
@@ -224,7 +224,7 @@
           // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
         });
         wx.checkJsApi({
-          jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+          jsApiList: ['scanQRCode'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
           success: function(res) {
             // 以键值对的形式返回，可用的api值true，不可用为false
             // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
